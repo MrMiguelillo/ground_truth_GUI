0be1a39957f0dc3fed9561bc1d9afda3853e3f5c
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-14 17:12:37 +0200

    circles drawn on mouse clicks, change in coordinates when image scales is pending

--- win_test_pygame.py
+++ win_test_pygame.py
@@ -13,6 +13,8 @@ doc_img = pygame.image.load(r"C:\Users\usuario\Desktop\document\1823-L119.M3\117
 
 scale = 1
 originx, originy = (0, 0)
+pt1x, pt1y = (0, 0)
+pt2x, pt2y = (0, 0)
 movement_speed = 10
 clock = pygame.time.Clock()
 
@@ -21,21 +23,29 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                temp_coords = pygame.mouse.get_pos()
+                pt1x, pt1y = ((temp_coords[0] - originx), (temp_coords[1] - originy))
+            if event.button == 3:
+                temp_coords = pygame.mouse.get_pos()
+                pt2x, pt2y = ((temp_coords[0] - originx), (temp_coords[1] - originy))
             if event.button == 4:
                 scale += 0.05
             elif event.button == 5 and scale > 0:
                 scale -= 0.05
 
     if pygame.key.get_pressed()[pygame.K_LEFT] != 0:
-        originx -= movement_speed  # * int(1 / scale)
+        originx -= movement_speed
     elif pygame.key.get_pressed()[pygame.K_RIGHT] != 0:
-        originx += movement_speed  # * int(1 / scale)
+        originx += movement_speed
     if pygame.key.get_pressed()[pygame.K_UP] != 0:
-        originy -= movement_speed  # * int(1/scale)
+        originy -= movement_speed
     elif pygame.key.get_pressed()[pygame.K_DOWN] != 0:
-        originy += movement_speed  # * int(1 / scale)
+        originy += movement_speed
 
     image = pygame.transform.scale(doc_img, (int(doc_img.get_width() * scale), int(doc_img.get_height() * scale)))
+    pygame.draw.circle(image, (255, 0, 0), (pt1x, pt1y), 5)
+    pygame.draw.circle(image, (0, 0, 255), (pt2x, pt2y), 5)
 
     screen.fill(black)
     screen.blit(image, (originx, originy))
