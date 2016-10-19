9776f4c00fb57c88310ffdc2426cfd07845bd1cc
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-19 17:32:44 +0200

    tkinter control panel basic layout done. functionality pending

--- win_test_tkinter.py
+++ win_test_tkinter.py
@@ -1,14 +1,55 @@
 import tkinter as tk
-from tkinter import messagebox
+# from tkinter import messagebox
 
-top = tk.Tk()
+root = tk.Tk()
+root.geometry("400x300+30+30")
 
-def helloCallBack():
-   tk.messagebox.showinfo("Hello Python", "Hello World")
+# POINT 1 DISPLAY
+pt1_label = tk.Label(root, text="Point 1:")
+pt1_label.place(x=20, y=10)
 
-B = tk.Button(top, text="Hello", command=helloCallBack)
+pt1x_value = tk.StringVar()
+pt1x_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt1x_value)
+pt1x_info.place(x=70, y=10, width=50)
+pt1x_value.set("hola")
 
-B.pack()
-top.mainloop()
+pt1y_value = tk.StringVar()
+pt1y_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt1y_value)
+pt1y_info.place(x=130, y=10, width=50)
+pt1y_value.set("hola")
+
+
+# POINT 2 DISPLAY
+pt2_label = tk.Label(root, text="Point 2:")
+pt2_label.place(x=20, y=30)
+
+pt2x_value = tk.StringVar()
+pt2x_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt2x_value)
+pt2x_info.place(x=70, y=30, width=50)
+pt2x_value.set("hola")
+
+pt2y_value = tk.StringVar()
+pt2y_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt2y_value)
+pt2y_info.place(x=130, y=30, width=50)
+pt2y_value.set("hola")
+
+
+# SEAL TYPE LIST
+type_label = tk.Label(root, text="Type:")
+type_label.place(x=20, y=70)
+
+OPTIONS = [
+    "egg",
+    "bunny",
+    "chicken"
+]
+
+seal_type = tk.StringVar(root)
+seal_type.set(OPTIONS[0])  # initial value
+
+option = tk.OptionMenu(root, seal_type, *OPTIONS)
+option.place(x=55, y=70)
+
+root.mainloop()
 
 # TODO: Create control panel
