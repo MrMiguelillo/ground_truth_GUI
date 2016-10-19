86b341dbd66e81a558dd66cea64aac6145f22a76
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-19 18:31:59 +0200

    fixed seal. python does not support method overloading

--- seal.py
+++ seal.py
@@ -1,10 +1,5 @@
 class Seal:
-    def __init__(self):
-        self.img_route = ''
-        self.name = ''
-        self.author = ''
-
-    def __init__(self, img_route, name, author):
+    def __init__(self, img_route='', name='', author=''):
         self.img_route = img_route
         self.name = name
         self.author = author
