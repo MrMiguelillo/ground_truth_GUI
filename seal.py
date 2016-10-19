47d7c377019a498301bee03e34ee1124ee48e587
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-19 17:34:22 +0200

    seal class created

---
+++ seal.py
@@ -0,0 +1,10 @@
+class Seal:
+    def __init__(self):
+        self.img_route = ''
+        self.name = ''
+        self.author = ''
+
+    def __init__(self, img_route, name, author):
+        self.img_route = img_route
+        self.name = name
+        self.author = author
