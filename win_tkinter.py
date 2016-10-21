4bb7a37bd483070adacf6cfcf296880cc83ba2a6
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-21 16:48:11 +0200

    win_tkinter modified to be a class instead

--- win_tkinter.py
+++ win_tkinter.py
@@ -3,49 +3,55 @@ import database
 import widgets
 
 
-root = tk.Tk()
-root.geometry("700x570+30+30")
+class WinTkinter:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("700x570+30+30")
+        
+        self.db = database.Database('docs_osborne', 'testuser', 'test123', ('sellos', 'documentos'))
+        self.db.load_seals()
+        
+        # POINT 1 DISPLAY
+        self.pt1_label = tk.Label(self.root, text="Point 1:")
+        self.pt1_label.place(x=20, y=10)
+        
+        self.pt1x_value = tk.StringVar()
+        self.pt1x_info = tk.Entry(self.root, state=tk.DISABLED, textvariable=self.pt1x_value)
+        self.pt1x_info.place(x=70, y=10, width=50)
+        self.pt1x_value.set("hola")
+        
+        self.pt1y_value = tk.StringVar()
+        self.pt1y_info = tk.Entry(self.root, state=tk.DISABLED, textvariable=self.pt1y_value)
+        self.pt1y_info.place(x=130, y=10, width=50)
+        self.pt1y_value.set("hola")
 
-db = database.Database('docs_osborne', 'testuser', 'test123', ('sellos', 'documentos'))
-db.load_seals()
+        # POINT 2 DISPLAY
+        self.pt2_label = tk.Label(self.root, text="Point 2:")
+        self.pt2_label.place(x=20, y=30)
+        
+        self.pt2x_value = tk.StringVar()
+        self.pt2x_info = tk.Entry(self.root, state=tk.DISABLED, textvariable=self.pt2x_value)
+        self.pt2x_info.place(x=70, y=30, width=50)
+        self.pt2x_value.set("hola")
+        
+        self.pt2y_value = tk.StringVar()
+        self.pt2y_info = tk.Entry(self.root, state=tk.DISABLED, textvariable=self.pt2y_value)
+        self.pt2y_info.place(x=130, y=30, width=50)
+        self.pt2y_value.set("hola")
 
-# POINT 1 DISPLAY
-pt1_label = tk.Label(root, text="Point 1:")
-pt1_label.place(x=20, y=10)
-
-pt1x_value = tk.StringVar()
-pt1x_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt1x_value)
-pt1x_info.place(x=70, y=10, width=50)
-pt1x_value.set("hola")
-
-pt1y_value = tk.StringVar()
-pt1y_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt1y_value)
-pt1y_info.place(x=130, y=10, width=50)
-pt1y_value.set("hola")
-
-
-# POINT 2 DISPLAY
-pt2_label = tk.Label(root, text="Point 2:")
-pt2_label.place(x=20, y=30)
-
-pt2x_value = tk.StringVar()
-pt2x_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt2x_value)
-pt2x_info.place(x=70, y=30, width=50)
-pt2x_value.set("hola")
-
-pt2y_value = tk.StringVar()
-pt2y_info = tk.Entry(root, state=tk.DISABLED, textvariable=pt2y_value)
-pt2y_info.place(x=130, y=30, width=50)
-pt2y_value.set("hola")
+        # SEAL SELECTION ITEMS
+        self.seal_type_list = widgets.SealsList(self.root, self.db)
+        # self.seal_type_list.init_option_menu()
+        self.seal_type_list.x = 20
+        self.seal_type_list.y = 70
+        self.seal_type_list.place_items()
 
 
-# SEAL SELECTION ITEMS
-seal_type_list = widgets.SealsList(root, db)
-# seal_type_list.init_option_menu()
-seal_type_list.x = 20
-seal_type_list.y = 70
-seal_type_list.place_items()
+if __name__ == "__main__":
+    win = WinTkinter()
+    while 1:
+        win.root.update_idletasks()
+        win.root.update()
 
-root.mainloop()
-
+# TODO: Transform this into a class
 # TODO: add 'new seal' button plus its corresponding form
