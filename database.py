2f2074e1bf9814d052ea7f6ad375b0d58e2ec8f9
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-23 14:21:17 +0200

    new method for inserting new seals

--- database.py
+++ database.py
@@ -22,5 +22,17 @@ class Database:
                 route = row[3]
                 new_seal = seal.Seal(route, name, author)
                 self.seal_list.append(new_seal)
-        except:
-            print("error: unable to fetch seals data")
+        except RuntimeError:
+            print("error: unable to fetch seals data\n")
+
+    def insert_seal(self, name, author):
+        path = "C:/Users/usuario/Desktop/new_base/" + name + ".png"
+        sql = """insert into %s(nombre, autor, ruta, añadido_manual)
+                 values ('%s', '%s', '%s', 1)""" % (self.table_names[0], name, author, path)
+
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except RuntimeError:
+            self.db.rollback()
+            print("unable to insert data into database\n")
