22ed7e2f9a10df0efca656805c825fb5115ef318
Miguel A.R <miguelrodrigo249@gmail.com>
2016-10-24 19:15:12 +0200

    integration now builds list of root paths which have images inside in order to feed the GUI

--- pygame_tkinter_integration.py
+++ pygame_tkinter_integration.py
@@ -1,6 +1,20 @@
-import win_tkinter
+import os
 import win_pygame
 
+
+path = 'C:/Users/usuario/Desktop/Document'
+walk = os.walk(path)
+
+doc_paths = []
+for root, dirs, files in walk:
+    there_is_any_img = False
+    for curr_file in files:
+        if curr_file.endswith(".png"):
+            there_is_any_img = True
+
+    if there_is_any_img:
+        root = root.replace("\\", "/")
+        doc_paths.append(root)
 
 doc_win = win_pygame.WinPygame()
 # control_panel = win_tkinter.WinControlPanel()
